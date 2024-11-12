a8f0d7b555c732c54c60b3985fdcedbff9f37be5
trescero <drksam@outlook.com>
2024-11-12 12:00:23 -0500

    Avancement

---
+++ Assets/Sandbox/DD/UiInteraction.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiInteraction : MonoBehaviour
+{
+    private void onClick(){
+        Debug.Log("yooooooooo gang vorp");
+    }
+}
