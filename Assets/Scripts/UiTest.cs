74c0c42d42828e815fc455a9fafdccb06b22ee2d
trescero <drksam@outlook.com>
2024-11-26 09:54:40 -0500

    Fonctionalité base button

---
+++ Assets/Scripts/UiTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiTest : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("yooooo");
+    }
+}
