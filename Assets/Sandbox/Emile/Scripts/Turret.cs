639e11be3e86c12bb8b409efa3fbf5cb86d6f74e
Hébert Émile <hebert.emile96@gmail.com>
2024-12-16 19:54:10 -0500

    Ajout de sons

--- Assets/Sandbox/Emile/Scripts/Turret.cs
+++ Assets/Sandbox/Emile/Scripts/Turret.cs
@@ -8,7 +8,7 @@ public class Turret : MonoBehaviour
 
 	private Transform target;
 	private Enemy targetEnemy;
-    //public AudioSource fireSound;
+    public AudioSource fireSound;
 
 	[Header("General")]
 	public GameObject batterie;
@@ -75,8 +75,8 @@ public class Turret : MonoBehaviour
 		GameObject bulletGO = (GameObject)Instantiate(tourrellesValues.prefabProjectile, firePoint.position, firePoint.rotation);
 		Bullet bullet = bulletGO.GetComponent<Bullet>();
 
-		//fireSound.Play();
-
+		fireSound.Play();
+		Debug.Log("shoot");
 
 		if (bullet != null)
 			bullet.Seek(target);
