655e9b5c7c47d7a2520077aa98dc8ea268e065a1
trescero <drksam@outlook.com>
2024-12-16 23:02:22 -0500

    niveau final, ajustements game manager

--- Assets/Sandbox/DD/GameManager/GameManager.cs
+++ Assets/Sandbox/DD/GameManager/GameManager.cs
@@ -5,15 +5,39 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-
     [SerializeField] private TMP_Text _textMoney;
     [SerializeField] private TMP_Text _textPoints;
     [SerializeField] private Player_ScriptableObject _player;
 
+    [SerializeField] private GameObject uiPointage;
+
     private void Update()
     {
-        _textMoney.text = "$: " + _player.money.ToString();
-        _textPoints.text = "Points: " + _player.points.ToString();
+        if (uiPointage != null && uiPointage.activeSelf)
+        {
+            UpdateUI();
+        }
     }
-    
+
+    private void Start()
+    {
+        if (uiPointage != null && uiPointage.activeSelf)
+        {
+            UpdateUI();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        _textMoney.text = "$: " + _player.money.ToString();
+        _textPoints.text = "Pointage: " + _player.points.ToString();
+    }
+
+    public void ResetPlayerValues()
+    {
+        _player.money = 0;
+        _player.points = 0;
+
+        UpdateUI();
+    }
 }
